ec98b0b2fc64edf7ac942e25066cf02f2c52b3c5
Senpai <senpai@mail.ru>
2020-11-17 23:48:18 +0300

    Lesson 7 + Search Short Path with BFS

--- src/ru/geekbrains/ads/lesson7/homework/SearchShortPath.java
+++ src/ru/geekbrains/ads/lesson7/homework/SearchShortPath.java
@@ -1,4 +1,39 @@
 package ru.geekbrains.ads.lesson7.homework;
 
+import ru.geekbrains.ads.lesson7.Graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class SearchShortPath {
+    public static void main(String[] args) {
+
+        Graph graph = new Graph(10);
+        graph.addVertex("Москва");
+        graph.addVertex("Тула");
+        graph.addVertex("Рязань");
+        graph.addVertex("Калуга");
+        graph.addVertex("Липецк");
+        graph.addVertex("Тамбов");
+        graph.addVertex("Орел");
+        graph.addVertex("Саратов");
+        graph.addVertex("Курск");
+        graph.addVertex("Воронеж");
+
+        graph.addEdges("Москва", "Тула", "Рязань", "Калуга");
+        graph.addEdges("Тула", "Липецк");
+        graph.addEdges("Рязань", "Тамбов");
+        graph.addEdges("Калуга", "Орел");
+        graph.addEdges("Тамбов", "Саратов");
+        graph.addEdges("Воронеж", "Липецк", "Саратов", "Курск");
+
+        graph.searchPath("Москва", "Воронеж");
+        graph.searchPath("Москва", "Тамбов");
+        graph.searchPath("Тула", "Курск");
+    }
+
+
+
+
 }
